8c52b845c48230cf24e13c3a2f9e27f1d060109b
Akshay Ausekar <99523912+Akshay-1-7@users.noreply.github.com>
2022-08-21 09:21:57 +0530

    Update grid.js

--- grid.js
+++ grid.js
@@ -1,1 +1,16 @@
 console.log("hello  world");
+var mazeContainer = document.getElementById("maze_container");
+
+var size = 10;
+
+for(var i = 0; i < size; i++){
+  var row = document.createElement("div");
+  row.id = "row"+(i+1);
+  for(var j = 0; j < size; j++){
+    var col = document.createElement("div");
+    col.id = "col"+((i*10)+(i+j)); 
+    row.appendChild(col);
+  }
+  mazeContainer.appendChild(row);
+}
+
